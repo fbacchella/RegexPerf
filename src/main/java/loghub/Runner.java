e840c696ceec88ba4508830894549288119df9d9
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2020-09-07 14:54:48 +0200

    Improving runbench, - using a blackhole, - adding a check for the catastrophic parsing of (x+x+)+y - addig the check of a log line, but needs to be improved for values extraction.

--- src/main/java/loghub/Runner.java
+++ src/main/java/loghub/Runner.java
@@ -22,8 +22,10 @@ package loghub;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.infra.Blackhole;
 
 import com.google.common.base.Strings;
 
@@ -34,7 +36,9 @@ public abstract class Runner<P> {
             "(([^:]+)://)?([^:/]+)(:([0-9]+))?(/.*)", // URL match without starting ^
             "usd [+-]?[0-9]+.[0-9][0-9]", // Canonical US dollar amount
             "\\b(\\w+)(\\s+\\1)+\\b", // Duplicate words
-            "\\{(\\d+):((.(?!-}))*).*" // Long matches
+            "(x+x+)+y", // Catastrophic Backtracking
+            "\\{(\\d+):((.(?!-}))*).*", // Long matches
+            "\\[(?<time>\\d{4}[\\/-]\\d{2}[\\/-]\\d{2}[\\-\\s]\\d{2}:\\d{2}:\\d{2}([\\.,]\\d+)?)\\]\\s+\\[\\s?(?<severity>(?i)TRACE|DEBUG|PERF|NOTE|INFO|WARN|ERROR|FATAL)\\s?\\]\\s+\\[(?<thread>([\\\"\\w\\d\\.\\,\\-_\\@\\s\\/\\:\\#\\\\\\=\\{\\}\\&\\+\\%\\)\\(]*)((\\.\\.\\.\\[).*(ing\\]))?)\\]\\s+\\[(?<logger>[\\w\\d\\.\\-_]*)\\]\\s(?<message>.*)"
     };
 
     private static final String[] strings = {
@@ -43,10 +47,13 @@ public abstract class Runner<P> {
             "usd 1234.00",
             "he said she said he said no",
             "same same same",
-            "{1:" + Strings.repeat("this is some more text - and some more and some more and even more ", 10) + "-}\n"
+            "xxxxxxxxxxxxxxxxy", //Succeed backtracking
+            "xxxxxxxxxxxxxxxxx", //Failing backtracking
+            "{1:" + Strings.repeat("this is some more text - and some more and some more and even more ", 10) + "-}\n",
+            "[2020-07-20 09:35:41,099] [INFO ] [1637163705@thread-name-3284] [loghub.dummy.class] A found String."
     };
 
-    private static boolean[][] expectedMatch = new boolean[5][6];
+    private static boolean[][] expectedMatch = new boolean[7][9];
 
     static
     {
@@ -56,30 +63,63 @@ public abstract class Runner<P> {
         expectedMatch[0][3] = false;
         expectedMatch[0][4] = false;
         expectedMatch[0][5] = false;
+        expectedMatch[0][6] = false;
+        expectedMatch[0][7] = false;
+        expectedMatch[0][8] = false;
         expectedMatch[1][0] = true;
         expectedMatch[1][1] = true;
         expectedMatch[1][2] = false;
         expectedMatch[1][3] = false;
         expectedMatch[1][4] = false;
         expectedMatch[1][5] = false;
+        expectedMatch[1][6] = false;
+        expectedMatch[1][7] = false;
+        expectedMatch[1][8] = false;
         expectedMatch[2][0] = false;
         expectedMatch[2][1] = false;
         expectedMatch[2][2] = true;
         expectedMatch[2][3] = false;
         expectedMatch[2][4] = false;
         expectedMatch[2][5] = false;
+        expectedMatch[2][6] = false;
+        expectedMatch[2][7] = false;
+        expectedMatch[2][8] = false;
         expectedMatch[3][0] = false;
         expectedMatch[3][1] = false;
         expectedMatch[3][2] = false;
         expectedMatch[3][3] = false;
         expectedMatch[3][4] = true;
         expectedMatch[3][5] = false;
+        expectedMatch[3][6] = false;
+        expectedMatch[3][7] = false;
+        expectedMatch[3][8] = false;
         expectedMatch[4][0] = false;
         expectedMatch[4][1] = false;
         expectedMatch[4][2] = false;
         expectedMatch[4][3] = false;
         expectedMatch[4][4] = false;
         expectedMatch[4][5] = true;
+        expectedMatch[4][6] = false;
+        expectedMatch[4][7] = false;
+        expectedMatch[4][8] = false;
+        expectedMatch[5][0] = false;
+        expectedMatch[5][1] = false;
+        expectedMatch[5][2] = false;
+        expectedMatch[5][3] = false;
+        expectedMatch[5][4] = false;
+        expectedMatch[5][5] = false;
+        expectedMatch[5][6] = false;
+        expectedMatch[5][7] = true;
+        expectedMatch[5][8] = false;
+        expectedMatch[6][0] = false;
+        expectedMatch[6][1] = false;
+        expectedMatch[6][2] = false;
+        expectedMatch[6][3] = false;
+        expectedMatch[6][4] = false;
+        expectedMatch[6][5] = false;
+        expectedMatch[6][6] = false;
+        expectedMatch[6][7] = false;
+        expectedMatch[6][8] = true;
     }
 
     private final Map<String, P> patternsMap = new HashMap<>(patterns.length);
@@ -87,23 +127,45 @@ public abstract class Runner<P> {
     protected abstract P generate(String i);
     protected abstract boolean match(P pattern, String searched);
 
-    public void run() {
-        for (int regnum = 0; regnum < patterns.length - 1; regnum++) {
-            P pattern = patternsMap.get(patterns[regnum]);
-            assert pattern!= null : patterns[regnum] + " not found";
-            for (int strnum = 0; strnum < strings.length - 1; strnum++) {
+    public void run(Blackhole blackHole) {
+        for (int patnum = 0; patnum < patterns.length - 1; patnum++) {
+            P pattern = patternsMap.get(patterns[patnum]);
+            assert pattern != null : patterns[patnum] + " not found";
+            for (int strnum = 0; strnum < strings.length - 3; strnum++) {
                 boolean b = match(pattern, strings[strnum]);
-                assert (b == expectedMatch[regnum][strnum]) : String.format("%s %s: %s = %s ?", patterns[regnum], strings[strnum], b, expectedMatch[regnum][strnum]);
+                blackHole.consume(b);
+                assert (b == expectedMatch[patnum][strnum]) : String.format("[%d][%d] %s %s: %s = %s ?", patnum, strnum, patterns[patnum], strings[strnum], b, expectedMatch[patnum][strnum]);
             }
         }
     }
 
-    public void runbig() {
-        for (int regnum = 0; regnum < patterns.length; regnum++) {
-            P pattern = patternsMap.get(patterns[regnum]);
+    public void runbig(Blackhole blackHole) {
+        for (int patnum = 0; patnum < patterns.length - 1; patnum++) {
+            P pattern = patternsMap.get(patterns[patnum]);
+            int strnum = strings.length - 2;
+            boolean b = match(pattern, strings[strnum]);
+            blackHole.consume(b);
+            assert (b == expectedMatch[patnum][strnum]) : String.format("[%d][%d] %s %s: %s = %s ?", patnum, strnum, patterns[patnum], strings[strnum], b, expectedMatch[patnum][strnum]);
+        }
+    }
+
+    public void runcatastroph(Blackhole blackHole) {
+        for (int patnum = 0; patnum < patterns.length - 1; patnum++) {
+            P pattern = patternsMap.get(patterns[patnum]);
+            int strnum = strings.length - 3;
+            boolean b = match(pattern, strings[strnum]);
+            blackHole.consume(b);
+            assert (b == expectedMatch[patnum][strnum]) : String.format("[%d][%d] %s %s: %s = %s ?", patnum, strnum, patterns[patnum], strings[strnum], b, expectedMatch[patnum][strnum]);
+        }
+    }
+
+    public void runlog(Blackhole blackHole) {
+        for (int patnum = 0; patnum < patterns.length; patnum++) {
+            P pattern = patternsMap.get(patterns[patnum]);
             int strnum = strings.length - 1;
             boolean b = match(pattern, strings[strnum]);
-            assert (b == expectedMatch[regnum][strnum]) : String.format("%s %s: %s = %s ?", patterns[regnum], strings[strnum], b, expectedMatch[regnum][strnum]);
+            blackHole.consume(b);
+            assert (b == expectedMatch[patnum][strnum]) : String.format("[%d][%d] %s %s: %s = %s ?", patnum, strnum, patterns[patnum], strings[strnum], b, expectedMatch[patnum][strnum]);
         }
     }
 
@@ -111,8 +173,9 @@ public abstract class Runner<P> {
     public void prepare() {
         Arrays.stream(patterns).forEach( i -> {
             try {
-                patternsMap.put(i, generate(i));
+                Optional.ofNullable(generate(i)).ifPresent(p -> patternsMap.put(i, p));
             } catch (Exception e) {
+                // System.out.format("%s: %s -> %s\n", getClass().getName(), i, e.getMessage());
             }
         });
     }
--- src/test/java/loghub/RunnerTest.java
+++ src/test/java/loghub/RunnerTest.java
@@ -1,8 +1,10 @@
 package loghub;
 
 import org.junit.Test;
+import org.openjdk.jmh.infra.Blackhole;
 
 import loghub.states.State_com_basistech_tclre;
+import loghub.states.State_com_google_code_regexp;
 import loghub.states.State_com_google_re2j;
 import loghub.states.State_com_ibm_icu_text;
 import loghub.states.State_com_karneim_util_collection_regex;
@@ -12,6 +14,7 @@ import loghub.states.State_gnu_regexp;
 import loghub.states.State_gnu_rex;
 import loghub.states.State_io_thekraken_grok_api;
 import loghub.states.State_java_util_regex;
+import loghub.states.State_java_util_regex_reuse;
 import loghub.states.State_jregex;
 import loghub.states.State_kmy_regex_util;
 import loghub.states.State_monq_jfa;
@@ -26,120 +29,131 @@ import loghub.states.State_org_joni_utf16le;
 
 public class RunnerTest {
 
-    private void run(Runner<?> runner, boolean withbig) {
+    private void run(Runner<?> runner, boolean withbig, boolean withlog) {
+        Blackhole bh = new Blackhole("Today's password is swordfish. I understand instantiating Blackholes directly is dangerous.");
         runner.prepare();
-        runner.run();
+        runner.run(bh);
         if (withbig) {
-            runner.runbig();
+            runner.runbig(bh);
+        }
+        runner.runcatastroph(bh);
+        if (withlog) {
+            runner.runlog(bh);
         }
     }
 
     @Test(expected = AssertionError.class)
     public void com_basistech_tclre() {
-        run(new State_com_basistech_tclre(), true);
+        run(new State_com_basistech_tclre(), true, true);
     }
 
     @Test(expected = AssertionError.class)
     public void com_google_re2j() {
-        run(new State_com_google_re2j(), false);
+        run(new State_com_google_re2j(), false, true);
     }
 
     @Test(expected = AssertionError.class)
     public void com_ibm_icu_text() {
-        run(new State_com_ibm_icu_text(), false);
+        run(new State_com_ibm_icu_text(), false, true);
     }
 
-    @Test(expected = AssertionError.class)
+    @Test(expected = Error.class)
     public void com_karneim_util_collection_regex() {
-        run(new State_com_karneim_util_collection_regex(), false);
+        run(new State_com_karneim_util_collection_regex(), false, true);
     }
 
     @Test
     public void com_stevesoft_pat() {
-        run(new State_com_stevesoft_pat(), true);
+        run(new State_com_stevesoft_pat(), true, false);
     }
 
     @Test(expected = AssertionError.class)
     public void dk_brics_automaton() {
-        run(new State_dk_brics_automaton(), true);
+        run(new State_dk_brics_automaton(), false, false);
     }
 
     @Test
     public void gnu_regexp() {
-        run(new State_gnu_regexp(), false);
+        run(new State_gnu_regexp(), false, false);
     }
 
-    @SuppressWarnings("rawtypes")
-    @Test(expected = AssertionError.class)
     public void gnu_rex() {
-        run(new State_gnu_rex(), true);
+        run(new State_gnu_rex(), true, true);
     }
 
     @Test
     public void io_thekraken_grok_api() {
-        run(new State_io_thekraken_grok_api(), true);
+        run(new State_io_thekraken_grok_api(), true, true);
+    }
+
+    @Test
+    public void java_util_regex_reuse() {
+        run(new State_java_util_regex_reuse(), true, true);
     }
 
     @Test
     public void java_util_regex() {
-        run(new State_java_util_regex(), true);
+        run(new State_java_util_regex(), true, true);
     }
 
     @Test
     public void jregex() {
-        run(new State_jregex(), false);
+        run(new State_jregex(), false, false);
     }
 
-    @SuppressWarnings("rawtypes")
     @Test(expected = AssertionError.class)
     public void kmy_regex_util() {
-        run(new State_kmy_regex_util(), true);
+        run(new State_kmy_regex_util(), true, true);
     }
 
-    @SuppressWarnings("rawtypes")
     @Test(expected = AssertionError.class)
     public void monq_jfa() {
-        run(new State_monq_jfa(), true);
+        run(new State_monq_jfa(), true, true);
     }
 
     @Test
     public void org_apache_oro_text_regex() {
-        run(new State_org_apache_oro_text_regex(), false);
+        run(new State_org_apache_oro_text_regex(), false, false);
     }
 
-    @Test
+    @Test(expected = AssertionError.class)
     public void org_apache_regexp() {
-        run(new State_org_apache_regexp(), false);
+        run(new State_org_apache_regexp(), false, true);
     }
 
-    @Test()
+    @Test(expected = AssertionError.class)
     public void org_apache_xerces_impl_xpath_regex() {
-        run(new State_org_apache_xerces_impl_xpath_regex(), false);
+        run(new State_org_apache_xerces_impl_xpath_regex(), false, true);
     }
 
     @Test
     public void org_joni_ascii_reuse() {
-        run(new State_org_joni_ascii_reuse(), true);
+        run(new State_org_joni_ascii_reuse(), true, true);
     }
 
     @Test
     public void org_joni_ascii_unsafe() {
-        run(new State_org_joni_ascii_unsafe(), true);
+        run(new State_org_joni_ascii_unsafe(), true, true);
     }
 
     @Test
     public void org_joni_ascii() {
-        run(new State_org_joni_ascii(), true);
+        run(new State_org_joni_ascii(), true, true);
     }
 
     @Test
     public void org_joni() {
-        run(new State_org_joni(), true);
+        run(new State_org_joni(), true, true);
     }
 
     @Test
     public void org_joni_utf16le() {
-        run(new State_org_joni_utf16le(), true);
+        run(new State_org_joni_utf16le(), true, true);
+    }
+
+    @Test
+    public void com_google_code_regexp() {
+        run(new State_com_google_code_regexp(), true, true);
     }
 
 }
